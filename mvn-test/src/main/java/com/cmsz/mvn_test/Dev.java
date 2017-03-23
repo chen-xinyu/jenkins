cbd7e30f35456f75c4a3dfb2bd8c0be3b82e2813
chenxy <1063975796@qq.com>
2017-03-23 16:20:14 +0800

    new row

--- mvn-test/src/main/java/com/cmsz/mvn_test/Dev.java
+++ mvn-test/src/main/java/com/cmsz/mvn_test/Dev.java
@@ -5,6 +5,7 @@ public class Dev {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is a new branch!");
+		System.out.println("this is a new row");
 	}
 
 }
