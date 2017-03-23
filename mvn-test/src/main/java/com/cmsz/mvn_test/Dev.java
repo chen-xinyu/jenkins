169c1ede43e767c1ce5843d2e1545e61469c27ef
chenxy <1063975796@qq.com>
2017-03-23 16:13:18 +0800

    create a new branch

---
+++ mvn-test/src/main/java/com/cmsz/mvn_test/Dev.java
@@ -0,0 +1,10 @@
+package com.cmsz.mvn_test;
+
+public class Dev {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is a new branch!");
+	}
+
+}
